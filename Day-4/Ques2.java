8ffcc405274ebba9f70df7bb049a9f25fe52fcab
MohdAltamish <altamish6589@gmail.com>
2026-06-04 14:21:24 +0530

    Day-4: Add Ques2

---
+++ Day-4/Ques2.java
@@ -0,0 +1,23 @@
+// Write a program to Find nth Fibonacci term. 
+
+import java.util.Scanner;
+
+public class Ques2 {
+    public static int fibonacci(int num){
+        if(num <= 1){
+            return num;
+        }
+        return fibonacci(num - 1) + fibonacci(num - 2);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n = sc.nextInt();
+        System.out.print("The nth Fibonacci term is: ");
+        System.out.println(fibonacci(n));
+        
+
+        sc.close();
+    }
+}
