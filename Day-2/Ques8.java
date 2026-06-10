0e95828a2f42a16a6c01db6938c2cfdcbbcd46ef
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-2: Add Ques8 solution

---
+++ Day-2/Ques8.java
@@ -0,0 +1,27 @@
+// Write a program to Check whether a number is palindrome.
+import java.util.Scanner;
+
+public class Ques8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+        int rev = 0;
+        int original = num;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == rev) {
+            System.out.println("The number is a palinrome: ");
+        } else {
+            System.out.println("The number is not a palinrome: ");
+
+        }
+
+        sc.close();
+    }
+}
