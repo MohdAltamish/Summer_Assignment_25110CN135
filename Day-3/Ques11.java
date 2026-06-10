3ef0e848ad92d173d9ba535f6bd76499e4e31aa8
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-3: Add Ques11 solution

---
+++ Day-3/Ques11.java
@@ -0,0 +1,18 @@
+// Write a program to Find GCD of two numbers.
+
+import java.util.Scanner;
+
+public class Ques11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter Two Number: ");
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        while(B != 0){
+            int temp = B;
+            B = A % B;
+            A = temp;
+        }
+        System.out.println("The Greatest Comman Divisior is: " + A);
+    }
+}
