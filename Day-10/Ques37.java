8a1d83c499bba53408db25c1e1963cb944f80619
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:24 +0530

    Day-10: Add Ques37 solution

---
+++ Day-10/Ques37.java
@@ -0,0 +1,28 @@
+// Write a program to Print star pyramid.
+import java.util.*;
+
+public class Ques37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
