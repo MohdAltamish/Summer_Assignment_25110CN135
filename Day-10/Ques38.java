ebafbf7cc8ff82dbd7c6f1734937aed8e9900e77
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-10: Add Ques38 solution

---
+++ Day-10/Ques38.java
@@ -0,0 +1,28 @@
+// Write a program to Print reverse star pyramid.
+import java.util.*;
+
+public class Ques38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
