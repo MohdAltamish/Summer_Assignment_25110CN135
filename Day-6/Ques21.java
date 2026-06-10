c7d014b913e5cb912fe72fff76c8269baef9285b
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-6: Add Ques21 solution

---
+++ Day-6/Ques21.java
@@ -0,0 +1,21 @@
+// Write a program to Count set bits in a number. 
+
+import java.util.*;
+
+public class Ques21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a binary number; ");
+        int bnum = sc.nextInt();
+        int count = 0;
+        while(bnum > 0){
+            int rem = bnum % 2;
+            if(rem == 1){
+                count++;
+            }
+            bnum /= 10;
+        }
+        System.out.println("The give number has bits: " + count);
+        sc.close();
+    }
+}
