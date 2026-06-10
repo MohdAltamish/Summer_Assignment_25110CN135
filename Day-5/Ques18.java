573c82582a92c2ca16f136fac420ad7fa5c36301
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-5: Add Ques18 solution

---
+++ Day-5/Ques18.java
@@ -0,0 +1,35 @@
+// Write a program to Check Strong number
+import java.util.Scanner;
+
+public class Ques18 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num = sc.nextInt();
+
+        int org = num;
+        int sum = 0;
+
+        while(num > 0){
+            int digit = num % 10;
+            int fact = 1;
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+            sum += fact;
+            num /= 10;
+        } 
+    
+        if(sum == org){
+            System.out.println("The number is a strong number:");
+        }
+        else{
+            System.out.println("The number is not a strong number:");
+        }
+
+        sc.close();
+    }
+}
+
+
