33d45017eb2f8b7a8178506ad0afe9160364b960
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-9: Add Ques35 solution

---
+++ Day-9/Ques35.java
@@ -0,0 +1,24 @@
+// Write a program to Print repeated character pattern.
+
+import java.util.*;
+
+public class Ques35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            char ch = (char) ('A' + i - 1);
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
