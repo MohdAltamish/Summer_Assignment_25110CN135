cd868aa63595aaaa2581795da882a144a5509d95
MohdAltamish <altamish6589@gmail.com>
2026-06-10 13:48:25 +0530

    Day-9: Add Ques36 solution

---
+++ Day-9/Ques36.java
@@ -0,0 +1,26 @@
+// Write a program to Print hollow square pattern.
+import java.util.*;
+
+public class Ques36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of square: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
